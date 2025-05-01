d929a0646e7a529cffd9becb6c3ac7267638e220
Naveen Kumar <nkumar@opentext.com>
2025-05-01 13:09:53 +0530

    feat: Upgrade Astro NVIM to v5 from v4

--- home/dot_config/nvim/lua/community.lua
+++ home/dot_config/nvim/lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,9 +9,8 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.comment-box-nvim" },
   { import = "astrocommunity.indent.indent-rainbowline" },
-  -- :Codeium Auth | <C-g>: accept suggestion
-  -- { import = "astrocommunity.completion.codeium-vim" },
 }
